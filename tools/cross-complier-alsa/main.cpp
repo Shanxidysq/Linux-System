6349c5760c6188a1169b193ce99a7d865604cfc0
ouxin <177621116168@163.com>
2025-09-12 00:08:02 +0800

    增加交叉编译alsa示例

---
+++ tools/cross-complier-alsa/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <alsa/asoundlib.h>
+using namespace std;
+void Handler1()
+{
+    for (int i = 0; i < 100; ++i)
+    {
+        cout << i << endl;
+    }
+}
+
+void Handler2()
+{
+    for (int i = 110; i < 200; ++i)
+    {
+        cout << i << endl;
+    }
+}
+
+
+int main()
+{
+    std::thread th1(Handler1);
+    std::thread th2(Handler2);
+
+    th1.join();
+    th2.join();
+    return 0;
+}
